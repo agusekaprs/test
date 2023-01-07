57eb8a78d69a3949697a14fcac33c0f4c0988fec
Agus Eka Prasetya <agusekaprst@gmail.com>
2023-01-07 17:33:06 +0800

    Revert "resvert last 2 commit for fixing bugs"
    This reverts commit 1d40ab9f5444a8c0222b67183d62e99a42e0fc92.

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ app.listen(3020, () => {
   console.log("first commit");
   console.log("perubahan 1");
   console.log("perubahan 2");
+<<<<<<< HEAD
   console.log("perubahan 3 dev branch");
+=======
+  console.log("perubahan 3");
+  console.log("perubahan 4");
+>>>>>>> parent of 1d40ab9 (resvert last 2 commit for fixing bugs)
 });
